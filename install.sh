9637a81508d31a289320837ebded5ea21a18c725
kyrasarii <kyrasarii@gmail.com>
2025-08-09 13:32:58 +0300

    change default browser to firefox update packages

--- install.sh
+++ install.sh
@@ -16,11 +16,11 @@ makepkg -si --noconfirm
 # Install packages with pacman
 echo "Updating system and installing main packages with pacman..."
 sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm bluez bluez-utils brightnessctl btop btrfs-progs dkms dunst efibootmgr feh firefox fish fuzzel fzf git grim gst-plugin-pipewire htop hyprland hyprpicker imagemagick intel-ucode iwd kitty libpulse libva-nvidia-driver linux linux-firmware linux-headers ly neovim network-manager-applet networkmanager npm nvidia-dkms pipewire pipewire-alsa pipewire-jack pipewire-pulse playerctl polkit-kde-agent power-profiles-daemon python-gobject qt5-wayland qt5ct qt6-wayland qt6ct slurp smartmontools sof-firmware swww ttf-fira-code unzip uwsm vim waybar wget wireless_tools wireplumber xdg-desktop-portal-hyprland xdg-utils xorg-server xorg-xinit yazi zenity zip zram-generator
+sudo pacman -S --noconfirm bluez bluez-utils kvantum nwg-look brightnessctl btop btrfs-progs libreoffice-fresh dkms dunst efibootmgr feh fish fuzzel fzf git grim gst-plugin-pipewire htop hyprland hyprpicker imagemagick intel-ucode iwd kitty libpulse libva-nvidia-driver linux linux-firmware linux-headers ly neovim network-manager-applet networkmanager npm nvidia-dkms pipewire pipewire-alsa pipewire-jack pipewire-pulse playerctl polkit-kde-agent power-profiles-daemon python-gobject qt5-wayland qt5ct qt6-wayland qt6ct slurp smartmontools sof-firmware swww ttf-fira-code unzip uwsm vim waybar wget wireless_tools wireplumber xdg-desktop-portal-hyprland xdg-utils xorg-server xorg-xinit yazi zenity zip zram-generator
 
 # Install AUR packages with paru
 echo "Installing AUR packages with paru..."
-paru -S --noconfirm adw-gtk-theme kvantum nwg-look pfetch wallust wlogout
+paru -S --noconfirm adw-gtk-theme pfetch wallust wlogout tty-clock zen-browser-bin
 
 # Copy config files
 echo "Copying configuration files..."
